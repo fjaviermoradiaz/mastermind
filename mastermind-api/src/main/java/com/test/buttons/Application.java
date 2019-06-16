f19d77b44841e50a2d64dc85bccdd939db858ee9
fjaviermoradiaz <fjaviermoradiaz@gmail.com>
2019-06-16 14:51:31 +0200

    add mongo dependency and mongodb properties (application.yml)

--- mastermind-api/src/main/java/com/test/buttons/Application.java
+++ mastermind-api/src/main/java/com/test/buttons/Application.java
@@ -1,18 +1,12 @@
 package com.test.buttons;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
 
 @SpringBootApplication(scanBasePackages = {"com.test.buttons"})
-@EnableScheduling
 public class Application {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
